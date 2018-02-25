82c375c331c02d2b54a2a1325222af8e81659401
Bryan0xFF <bjmejia@correo.url.edu.gt>
2018-02-25 10:48:53 -0600

    BinaryTree interface implemented

---
+++ EDD_LAB02_1252016_1053016/BinaryTree/Nodo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Nodo<T>
+    {
+        public Nodo<T> Left { get; set; }
+        public Nodo<T> Right { get; set; }
+        public T Value { get; set; }
+
+    }
+}
