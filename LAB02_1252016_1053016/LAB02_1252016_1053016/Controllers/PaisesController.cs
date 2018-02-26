e753ce1916a9f81faf67e984665c899e54a8f452
AlexRod97 <rodriguezalex86@hotmail.com>
2018-02-26 14:49:24 -0600

    Implementación de base
    Codigo incluye la instancia de los controladores, modelo y algunas vistas

---
+++ LAB02_1252016_1053016/LAB02_1252016_1053016/Controllers/PaisesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BinTree;
+using LAB02_1252016_1053016.Models;
+using System.IO; 
+
+namespace LAB02_1252016_1053016.Controllers
+{
+    public class PaisesController : Controller
+    {
+        // GET: Paises
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //public ActionResult Cadenas()
+        //{
+        //    return RedirectToAction("Menu"); 
+        //}
+
+        //public ActionResult Enteros()
+        //{
+        //    return RedirectToAction("Menu");
+        //}
+
+        //public ActionResult Paises()
+        //{
+        //    return RedirectToAction("Menu");
+        //}
+
+        [HttpGet]
+        public ActionResult LecturaArchivo()
+        {
+            //aqui se abre una vista para poder subir el archivo
+            return View();
+        }
+
+       
+    }
+}
---
+++ LAB02_1252016_1053016/LAB02_1252016_1053016/Models/Pais.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LAB02_1252016_1053016.Models
+{
+    public class Pais
+    {
+        public string NombrePais { get; set; }
+        public string grupo  { get; set; }
+}
+}
